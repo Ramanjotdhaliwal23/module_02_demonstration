3b68b9ae614cd0e292ba5bd04870c1b0a6e685ac
Ramanjotdhaliwal23 <144178238+Ramanjotdhaliwal23@users.noreply.github.com>
2023-09-18 17:41:38 -0700

    Update assignment_02

--- module_02.py
+++ module_02.py
@@ -1,138 +1,147 @@
 """
-Description: Module 02 Demonstration
-Author: Ramanjot kaur
-Date: September 12,2023
-Usage: Run using button in the IDE
-(integrated development environment)
+Description: Created assignment 2
+Author: Amish
+Date: September 15, 2023
+Usage: For creating sdf assignment 2
 """
 
-#this is an incline comment.
+# Simple data types
+
+# Declare a variable to store a name and initialize it to your first name.
+first_name = "Amish"
+
+print(f"value: {first_name} type: {type(first_name).name}")
+
+# Declare a variable for Manitoba Driver's License status and initialize it.
+manitoba_license_status = True  
+
+print(f"value: {manitoba_license_status} type: {type(manitoba_license_status).name}")
+
+# Declare a variable to store the current year and initialize it.
+current_year = 2023
+
+print(f"this year: {current_year} type: {type(current_year).name}")
+
+# Add 1 to the year.
+current_year += 1
+
+print(f"next year: {current_year} type: {type(current_year).name}")
 
 
-'''
-This is a multiline comment.
-It spans over multiple lines.
-'''
 
-# constants (python doesn't really have constants)
-FEDERAL_TAX = 0.05
-PI = 3.14
+# Performing Calculations
 
+# Declare constants for GST and PST rates.
+GST_RATE = 0.08  
+PST_RATE = 0.16  
 
-absolute_value: abs(-12)
-print('absolute value:',absolute_value)
-print('absolute value:' +absolute_value)
-print('absolute value:', abs(-12 ) )
+# Declare a variable for the vehicle price and initialize it.
+vehicle_price = 100000.00  
 
-print("I am",'25', "years old!")
-print("apples", "oranges", "bananas", sep = "* ")
+# Calculate GST and PST taxes.
+gst_tax = vehicle_price * GST_RATE
+pst_tax = vehicle_price * PST_RATE
 
-#variables.
-name = 'rohini'
-age = 25
-pi = 3.14159
-number = 123
-is_a_student = True
+# Calculate the total cost including taxes.
+total_cost = vehicle_price + gst_tax + pst_tax
 
-print(type(name))
-print(type(age))
-print(type(pi))
-print(type(number))
-print(type(is_a_student))
+print(f"pre-tax value: {vehicle_price} PST: {pst_tax} GST: {gst_tax} total: {total_cost}")
+
+print(f"pre-tax value: ${vehicle_price:.2f} PST: ${pst_tax:.2f} GST: ${gst_tax:.2f} total: ${total_cost:.2f}")
 
 
-print(f"My name is{name} and I am{age} years old.")
-#output: My name is Rohini and I am 25 years
 
-print(f"The value of pi to two decimal places is {pi:.2f}.")
-print(f"The number is {number:05}")
-print(f"Hello,{name:> 10}")
+# Lists
 
+# Declare a list of numbers from 1 to 10.
+numbers_list = list(range(1, 11))
 
-#output" Hello,   Rohini
-print(f"Hello,{name:>10} ")
+print(f"Data type of the list: {type(numbers_list).name}")
 
+print("List of numbers:", numbers_list)
 
-#type conversion
-age=18
-current_salary = 67293.21
+# Add your first name to the list between values 5 and 6.
+numbers_list.insert(5, first_name)
 
-age_and_salary = age +current_salary
+print("Updated list:", numbers_list)
 
-print(age_and_salary, type(age_and_salary))
+# Remove the number 9 from the list.
+numbers_list.remove(9)
 
-months_old = "11"
-years_old = 25
+print("List after removing 9:", numbers_list)
 
-# age is implicity converted to integer
-#months_old is explicitly converted into float
-age = years_old + (float(months_old) / 12)
+# Create a second list.
+second_list = ['A', 'B', 'C']
 
-print(f"age as a float is{age:.3f}")
+# Create a third list containing values from the first and second lists.
+combined_list = numbers_list + second_list
 
-# $67,275.21
-print(f"salary is: ${current_salary:,.2f}")
-
-age = int(age)
-
-print(age)
-
-# maths
-# + addition, concatenation
-# - subtraction
-# * multiplication
-# / division
-
-# regular division
-result = 42/4  #10.5
-
-#integer division
-result = 42 // 4 #10
-
-# modulus
-result = 4 % 2   # 0
-result = 3 % 2   # 1
-result = 100 % 10  # 0
-result = 101 % 10   # 1
-
-# power
-result = 2 ** 4  # 16
-
-# assignment
-result = 1 + 1
-
-age = 25
-age = age + 1
-age += 1
-
-countdown = 100
-
-countdown -= 10
+print("Combined list:", combined_list)
 
 
-# shortcut operators
-# the variable must already have a value assigned
-# +=, -=, *=, /=, //=, %=, **=
 
-# order of operations
-# PEDMAS
-# BEDMAS
-# brackets or parentheses
-# exponents
-# Division
-# Multiplication
-# Addition
-# Subtraction
+# Tuples
 
-result = 10 + 5 * 2 / 3 - 1
+# Declare a tuple of Canadian provinces.
+canadian_provinces = ('Ontario', 'Quebec', 'British Columbia', 'Alberta')
 
-              # 10 / 3
-              # 3.33333
-              # 10 + 3.33333 - 1
-              # 12.3333
+print("Tuple of Canadian provinces:", canadian_provinces)
+print(f"Data type of the tuple: {type(canadian_provinces).name}")
 
-print(result)
 
-result  =  ((10 + 5) * 2) / 3 - 1 
-print(result)
 
+# Dictionaries
+
+# Declare a dictionary for Canadian currency values.
+currency_dict = {'nickel': 0.05, 'dime': 0.10, 'quarter': 0.25}
+
+print("Dictionary of currency values:", currency_dict)
+print(f"Data type of the dictionary: {type(currency_dict).name}")
+
+# Modify values to use whole numbers.
+for key in currency_dict:
+    currency_dict[key] = int(currency_dict[key] * 100)
+
+print("Modified dictionary of currency values:", currency_dict)
+
+# Add loonie and toonie.
+currency_dict['loonie'] = 100
+currency_dict['toonie'] = 200
+
+print("Dictionary with loonie and toonie:", currency_dict)
+
+
+
+
+# Sets
+
+# Declare a set of even numbers from 2 to 20.
+even_numbers = set(range(2, 21, 2))
+
+print("Set of even numbers:", even_numbers)
+print(f"Data type of the set: {type(even_numbers).name}")
+
+# Declare a set of multiples of 5 from 5 to 20.
+multiples_of_5 = set(range(5, 21, 5))
+
+print("Set of multiples of 5:", multiples_of_5)
+
+# Declare a set containing unique values from both sets.
+unique_values_set = even_numbers.union(multiples_of_5)
+
+print("Set of unique values:", unique_values_set)
+
+# Declare a set containing values in both sets.
+intersection_set = even_numbers.intersection(multiples_of_5)
+
+print("Set of values in both sets:", intersection_set)
+
+# Declare a set containing values in the first set but not in the second set.
+difference_set = even_numbers.difference(multiples_of_5)
+
+print("Set of values in the first set but not in the second set:", difference_set)
+
+# Declare a set containing values in the second set but not in the first set.
+difference_set_2 = multiples_of_5.difference(even_numbers)
+
+print("Set of values in the second set but not in the first set:", difference_set_2)
